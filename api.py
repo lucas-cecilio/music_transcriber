81e6e1707d230e5fbf3bc4f9a0f942e9e6219613
Lucas Cecilio <lucascecilio@poli.ufrj.br>
2024-09-09 15:50:45 -0300

    Updating paths

--- api.py
+++ api.py
@@ -3,14 +3,13 @@ import note_seq
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.responses import FileResponse
 from pathlib import Path
-from music_transcriber.utils import process_audio, load_model, transcript_audio, download_midi, plot_midi
+from music_transcriber.utils import process_audio, load_model, transcribe_audio, download_midi, plot_midi
 from music_transcriber.params import *
 
 app = FastAPI()
 
-UPLOAD_AUDIO_DIR = Path(os.path.join(BASE_DIR, 'input_audio'))
-MIDI_FILE_DIR = Path(os.path.join(BASE_DIR, 'outputs', 'midi_file'))
-MIDI_PLOT_DIR = Path(os.path.join(BASE_DIR, 'outputs', 'midi_plot'))
+# MIDI_FILE_DIR = Path(os.path.join(BASE_DIR, 'outputs', 'midi_file'))
+# MIDI_PLOT_DIR = Path(os.path.join(BASE_DIR, 'outputs', 'midi_plot'))
 
 # Endpoint to list available models
 @app.get("/available-models/")
@@ -27,7 +26,7 @@ async def upload_audio(file: UploadFile = File(...)):
         raise HTTPException(status_code=400, detail="Invalid file type.")
 
     # Saving uploaded file
-    file_location = UPLOAD_AUDIO_DIR / file.filename
+    file_location = INPUT_AUDIO_PATH / file.filename
     with open(file_location, "wb") as f:
         f.write(await file.read())
 
@@ -35,54 +34,59 @@ async def upload_audio(file: UploadFile = File(...)):
 
 # Transcribe audio with chosen model
 @app.post("/transcribe/")
-async def transcribe_audio(filename: str, model_type: str = "piano"):
+async def transcribe(filename: str, model_type: str = "piano"):
     if model_type not in AVAILABLE_MODELS:
         raise HTTPException(status_code=400, detail="Invalid model type. Choose from 'piano' or 'multi-instrument'.")
 
-    file_location = UPLOAD_AUDIO_DIR / filename
+    file_location = INPUT_AUDIO_PATH / filename
     if not file_location.exists():
         raise HTTPException(status_code=404, detail="File not found.")
 
+     # Process audio
+    audio_processed = process_audio(filename)
+    if audio_processed is None:
+        raise HTTPException(status_code=400, detail="Audio processing failed.")
+    
     # Load the model based on user choice
     selected_model = load_model(AVAILABLE_MODELS[model_type])
 
-    # Process audio
-    audio = process_audio(filename)
-    if audio is None:
-        raise HTTPException(status_code=400, detail="Audio processing failed.")
-
     # Run inference
-    est_ns = transcript_audio(selected_model, audio)
+    notes_sequence = transcribe_audio(selected_model, audio_processed)
 
     # Save the transcribed MIDI file
-    midi_filename = filename.replace(".wav", ".mid")
-    midi_filepath = MIDI_FILE_DIR / midi_filename
-    download_midi(est_ns)
+    midi_file_name = Path(filename).with_suffix(".mid")
+    midi_file_path = OUTPUT_MIDI_FILE_PATH / midi_file_name
+    download_midi(notes_sequence, midi_file_path)
+    
+    # Plot the notes sequence
+    midi_plot_name = midi_file_name.with_suffix(".png")
+    midi_plot_path = OUTPUT_MIDI_PLOT_PATH / midi_plot_name
+    plot_midi(notes_sequence, midi_plot_name, save_png=True)
 
-    return {"midi_filename": midi_filename, "midi_filepath": str(midi_filepath)}
+    return {"midi_file_name": midi_file_name, "midi_file_path": str(midi_file_path), "midi_plot_path": str(midi_plot_path)}
 
 # Download MIDI file
 @app.get("/download-midi/")
-async def download_midi_file(midi_filename: str):
-    midi_filepath = MIDI_FILE_DIR / midi_filename
-    if not midi_filepath.exists():
+async def download_midi_file(midi_file_name: str):
+    midi_file_path = OUTPUT_MIDI_FILE_PATH / midi_file_name
+    if not midi_file_path.exists():
         raise HTTPException(status_code=404, detail="MIDI file not found.")
 
-    return FileResponse(midi_filepath, media_type='audio/midi', filename=midi_filename)
+    return FileResponse(midi_file_path, media_type='audio/midi', filename=midi_file_name)
 
 # Plot MIDI file
-@app.get("/plot-midi/")
-async def plot_midi_file(midi_filename: str, save_png: bool = False):
-    midi_filepath = MIDI_FILE_DIR / midi_filename
-    if not midi_filepath.exists():
-        raise HTTPException(status_code=404, detail="MIDI file not found.")
+# @app.get("/plot-midi/")
+# async def plot_midi_file(midi_filename: str, save_png: bool = False):
+#     midi_filepath = MIDI_FILE_DIR / midi_filename
+#     if not midi_filepath.exists():
+#         raise HTTPException(status_code=404, detail="MIDI file not found.")
 
-    # Load and plot the MIDI file
-    est_ns = note_seq.midi_file_to_note_sequence(midi_filepath)
-    plot = plot_midi(est_ns, save_png=save_png)
+#     # Load and plot the MIDI file
+#     est_ns = note_seq.midi_file_to_note_sequence(midi_filepath)
+#     plot = plot_midi(est_ns, save_png=save_png)
 
-    if save_png:
-        plot_png_path = MIDI_PLOT_DIR / 'plot.png'
-        return {"plot_png_path": str(plot_png_path)}
+#     if save_png:
+#         plot_png_path = MIDI_PLOT_DIR / 'plot.png'
+#         return {"plot_png_path": str(plot_png_path)}
 
-    return {"message": "MIDI plot generated."}
+#     return {"message": "MIDI plot generated."}
--- music_transcriber/utils.py
+++ music_transcriber/utils.py
@@ -1,8 +1,8 @@
-import os
 import librosa
 import note_seq
 import subprocess
 
+from pathlib import Path
 from midi2audio import FluidSynth
 from selenium import webdriver
 from bokeh.io.export import export_png
@@ -20,7 +20,8 @@ def process_audio(audio_file: str):
         print("\n❌ Format not supported, only .wav or .mp3.")
         return None
 
-    audio_path = os.path.join(UPLOAD_AUDIO_DIR, audio_file)
+    audio_path = INPUT_AUDIO_PATH / audio_file
+    
     print('\nProcessing audio 🔄')
     audio_processed, _ = librosa.load(audio_path, sr=SAMPLE_RATE)
     print('\nAudio Processed ✅')
@@ -31,9 +32,10 @@ def process_audio(audio_file: str):
 def load_model(model_type: str):
     ''' TODO: write docstring'''
 
-    checkpoint_path = os.path.join(BASE_DIR, 'checkpoints', model_type)
+    checkpoint_model_path = os.path.join(CHECKPOINT_PATH, model_type)
     print('\nInitializing model 🔄')
-    model = InferenceModel(checkpoint_path, model_type=model_type)
+    
+    model = InferenceModel(checkpoint_path=checkpoint_model_path, model_type=model_type)
     print('\nModel initialized ✅')
     
     return model
@@ -49,15 +51,66 @@ def transcribe_audio(model, audio_processed):
     return notes_sequence
 
 
-def download_midi(notes_sequence):
+def download_midi(notes_sequence, midi_file_name):
     '''Saves the transcribed MIDI to a file.'''
     
-    midi_file_path = os.path.join(BASE_DIR, 'outputs', 'midi_file', 'transcribed.mid')
+    midi_file_path = OUTPUT_MIDI_FILE_PATH / midi_file_name
     
     print('\nDownloading midi 🔄')
     note_seq.sequence_proto_to_midi_file(notes_sequence, midi_file_path)
     
     print('\nThe midi file is ready! ✅')
+    
+def plot_midi(notes_sequence, midi_plot_name, save_png=True):
+    '''TODO: write docstring'''
+    
+    print('\nCreating a MIDI plot 🔄')
+    plot_midi = note_seq.plot_sequence(notes_sequence, show_figure=False)
+    midi_plot_path = OUTPUT_MIDI_PLOT_PATH / midi_plot_name
+    
+    # Add and adjust title
+    plot_midi.title.text = f'MIDI sequence of {str(midi_plot_name).replace(".png", "")}'
+    plot_midi.title.text_font_size = "20pt"
+    plot_midi.title.align = "center"
+    
+    # Adjust axis labels
+    plot_midi.xaxis.axis_label = "Time(s)" 
+    plot_midi.yaxis.axis_label = "Pitch Notes"
+    plot_midi.xaxis.axis_label_text_font_size = "16pt"
+    plot_midi.yaxis.axis_label_text_font_size = "16pt"  
+    
+    # Adjust the size of tick labels
+    plot_midi.xaxis.major_label_text_font_size = "14pt" 
+    plot_midi.yaxis.major_label_text_font_size = "14pt" 
+    
+    if save_png:
+        # Disable toolbar and change dimensions
+        plot_midi.toolbar_location = None  # Remove the toolbar
+        plot_midi.width = 1600  # Increase width for higher quality
+        plot_midi.height = 900  # Increase height for higher quality
+        
+        print('\nSaving a png of MIDI plot 📥')
+        save_plot_midi(plot_midi, midi_plot_path)
+        
+    print('\nMIDI plot done ✅')
+    return plot_midi
+    
+def save_plot_midi(plot_midi, midi_plot_path):
+    '''TODO: write docstring'''
+
+    # Configure Chrome driver in headless mode
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+
+    # Initialize the Chrome driver
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+
+    # Export the figure as a high-quality PNG
+    export_png(plot_midi, filename=midi_plot_path, webdriver=driver)
+
+    # Close the Chrome driver
+    driver.quit()
 
 def midi_to_audio(midi_filename: str):
     '''TODO: Write docstring'''
@@ -123,55 +176,4 @@ def midi_to_score(midi_filename: str):
 
 
 
-def plot_midi(notes_sequence, save_png=False):
-    '''TODO: write docstring'''
-    
-    print('\nCreating a MIDI plot 🔄')
-    plot_midi = note_seq.plot_sequence(notes_sequence, show_figure=False)
-    
-    # Add and adjust title
-    plot_midi.title.text = "MIDI"
-    plot_midi.title.text_font_size = "20pt"
-    plot_midi.title.align = "center"
-    
-    # Adjust axis labels
-    plot_midi.xaxis.axis_label = "Time(s)" 
-    plot_midi.yaxis.axis_label = "Pitch Notes"
-    plot_midi.xaxis.axis_label_text_font_size = "16pt"
-    plot_midi.yaxis.axis_label_text_font_size = "16pt"  
-    
-    # Adjust the size of tick labels
-    plot_midi.xaxis.major_label_text_font_size = "14pt" 
-    plot_midi.yaxis.major_label_text_font_size = "14pt" 
-    
-    if save_png:
-        # Disable toolbar and change dimensions
-        plot_midi.toolbar_location = None  # Remove the toolbar
-        plot_midi.width = 1600  # Increase width for higher quality
-        plot_midi.height = 900  # Increase height for higher quality
-        
-        print('\nSaving a png of MIDI plot 📥')
-        save_plot_midi(plot_midi)
-        
-    print('\nMIDI plot done ✅')
-    return plot_midi
-    
-def save_plot_midi(plot_midi):
-    '''TODO: write docstring'''
-
-    midi_plot_path = os.path.join(BASE_DIR, 'outputs', 'midi_plot', 'plot.png')
-
-    # Configure Chrome driver in headless mode
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-
-    # Initialize the Chrome driver
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-
-    # Export the figure as a high-quality PNG
-    export_png(plot_midi, filename=midi_plot_path, webdriver=driver)
-
-    # Close the Chrome driver
-    driver.quit()
     
